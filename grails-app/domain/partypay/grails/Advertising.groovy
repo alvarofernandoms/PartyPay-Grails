d406ce4d7c058abec5d903abb703e7ae4cd325eb
Álvaro Fernando <alvarofernandoms@hotmail.com>
2014-04-03 17:51:23 -0300

    Edit e creating default views for Advertising and Concourse

--- grails-app/domain/partypay/grails/Advertising.groovy
+++ grails-app/domain/partypay/grails/Advertising.groovy
@@ -2,6 +2,15 @@ package partypay.grails
 
 class Advertising {
 
+	String nameContractor
+	String advertisingMessage
+	Float value
+	Date startDate = new Date()
+	Date endDate = new Date()
+
+
     static constraints = {
+    	nameContractor blank: false, nullable: false
+    	startDate blank: false, nullable: false
     }
 }
--- grails-app/domain/partypay/grails/Concourse.groovy
+++ grails-app/domain/partypay/grails/Concourse.groovy
@@ -1,7 +1,12 @@
 package partypay.grails
 
-class Concourse {
+class Concourse extends Event {
+
+	String challenge
+	String prize
 
     static constraints = {
+    	challenge blank: false, nullable: false
+    	prize blank: false, nullable: false
     }
 }
