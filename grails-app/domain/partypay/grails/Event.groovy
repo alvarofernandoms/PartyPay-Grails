28112011d2096d5ba633f1921921865853cf16d1
cristianocs <engcristiano@hotmail.com.br>
2014-04-03 13:03:20 -0300

    Edit Class Event, Ticket

--- grails-app/domain/partypay/grails/Event.groovy
+++ grails-app/domain/partypay/grails/Event.groovy
@@ -2,6 +2,11 @@ package partypay.grails
 
 class Event {
 
+	String nameEvent
+	Date dateEvent
+	String adressEvent
+
     static constraints = {
+    	nameEvent blank: false, nullable: false
     }
 }
--- grails-app/domain/partypay/grails/Ticket.groovy
+++ grails-app/domain/partypay/grails/Ticket.groovy
@@ -2,6 +2,8 @@ package partypay.grails
 
 class Ticket {
 
+	Float ticketValue
+	
     static constraints = {
     }
 }
