5a29790e56d960ffb95e0f8657de0ef67d759134
Álvaro Fernando <alvarofernandoms@hotmail.com>
2014-04-03 21:05:18 -0300

    Integration with MySQL

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,8 +1,7 @@
 dataSource {
     pooled = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
+    driverClassName = "com.mysql.jdbc.Driver"
+    dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
 }
 hibernate {
     cache.use_second_level_cache = true
@@ -14,20 +13,25 @@ environments {
     development {
         dataSource {
             dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            url = "jdbc:mysql://localhost/partypay_grails?useUnicode=yes&characterEncoding=UTF-8"
+            username = "root"
+            password = "mysqlmysql"
         }
     }
     test {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            url = "jdbc:mysql://localhost/partypay_grails_prod?useUnicode=yes&characterEncoding=UTF-8"
+            username = "root"
+            password = "mysqlmysql"
         }
     }
     production {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-            pooled = true
+            url = "jdbc:mysql://localhost/partypay_grails_prod?useUnicode=yes&characterEncoding=UTF-8"
+            username = "root"
+            password = "mysqlmysql"
             properties {
                maxActive = -1
                minEvictableIdleTimeMillis=1800000
